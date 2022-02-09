22fdfa26e02000337b16bacd870a6edf58dd5d6a
Jonathan Tipton <nature_explorer98@outlook.com>
2022-02-09 10:43:23 -0700

    Updated build.sh for apk/ios builds

--- build.sh
+++ build.sh
@@ -1,11 +1,39 @@
 #!/bin/bash
-echo "Linux cannot compile the iOS version of the app, so this script will only
-build the Android APK."
+#echo "Linux cannot compile the iOS version of the app, so this script will only build the Android APK."
 
-flutter build apk
-if [ "$?" -ne "0" ]; then
-	  echo "Error with flutter build!"
-	  exit 1
+if [[ "$OSTYPE" =~ ^darwin ]]; then
+	echo "Creating Android APK build..."
+	flutter build apk
+	if [ "$?" -ne "0" ]; then
+          echo "Error with flutter apk build!"
+          exit 1
+	fi
+	
+	echo "Creating iOS build..."
+	flutter build ios
+	if [ "$?" -ne "0" ]; then
+          echo "Error with flutter iOS build!"
+          exit 1
+	fi
 fi
-echo "Build finished without errors."
+
+if [[ "$OSTYPE" =~ ^mysys ]]; then
+	echo "iOS build only supported on macOS! ONLY the Android build will be created!"
+        flutter build apk
+        if [ "$?" -ne "0" ]; then
+          echo "Error with flutter apk build!"
+          exit 1
+        fi
+fi
+
+if [[ "$OSTYPE" =~ ^linux ]]; then
+	echo "iOS build only supported on macOS! ONLY the Android build will be created!"
+        flutter build apk
+        if [ "$?" -ne "0" ]; then
+          echo "Error with flutter apk build!"
+          exit 1
+        fi
+fi
+
+echo "Build(s) finished without errors."
 exit 0
