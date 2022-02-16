605b2f8d31ede954254719821c07c9db48191a0c
andrewhaddon74 <86433050+AndrewHaddon74@users.noreply.github.com>
2022-02-15 19:07:45 -0700

    added unit testing

--- web-console/apps/authentication/tests.py
+++ web-console/apps/authentication/tests.py
@@ -4,5 +4,12 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 from django.test import TestCase
+from example import *
 
+class testTeaseCase(TestCase):
 # Create your tests here.
+    def test_testingpytest(self):
+        self.assertEqual(testingpytest(2, 2), 4)
+
+    def test_testingpytestbad(self):
+        self.assertEqual(testingpytest(2, 2), 5)
--- web-console/apps/home/tests.py
+++ web-console/apps/home/tests.py
@@ -6,3 +6,13 @@ Copyright (c) 2019 - present AppSeed.us
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase
+from example import *
+
+class testTeaseCase(TestCase):
+# Create your tests here.
+    def test_testingpytest(self):
+        self.assertEqual(testingpytest(2, 2), 4)
+
+    def test_testingpytestbad(self):
+        self.assertEqual(testingpytest(2, 2), 5)
