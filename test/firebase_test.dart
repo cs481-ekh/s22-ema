da8842d224bda577e47e29e44fade7acf5735fcb
AnonymousRecess <47878953+AnonymousRecess@users.noreply.github.com>
2022-02-15 21:39:12 -0700

    implements #6 (#29)
    closes #12

--- test/firebase_test.dart
+++ test/firebase_test.dart
@@ -1,50 +1,42 @@
-/// Uses https://pub.dev/packages/firebase_auth_mocks to test
-/// sign-in / register (?) UI
-
-import 'package:flutter_test/flutter_test.dart';
 import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
+import 'package:test/test.dart';
 
-import 'package:ema/main.dart';
+final tUser = MockUser(
+  isAnonymous: false,
+  uid: 'T3STU1D',
+  email: 'TestUser@testr.com',
+  displayName: 'Testing',
+  phoneNumber: '541 67 1234',
+  photoURL:
+      'https://cdn.pixabay.com/photo/2014/02/27/16/10/tree-276014__340.jpg',
+  refreshToken: 'test_token_1514',
+);
 
 void main() {
-  testWidgets('User can signin with valid credentials',
-      (WidgetTester tester) async {
-    final user = MockUser(
-        isAnonymous: false,
-        uid: 'uid123',
-        email: 'UnitTest@gmail.com',
-        displayName: 'ThomasTester');
-
+  test('Returns no user if not signed in', () async {
     final auth = MockFirebaseAuth();
-
-    // TODO: update for checking UI changes
-    final result = await auth.signInWithEmailAndPassword(
-        email: 'test1@gmail.com', password: "abc123");
-
-    print(result.user.toString());
-    final t = result.user;
-
-    // Verify 'Send Notification' button exists
-    expect(t?.displayName, 'ThomasTester');
+    final user = auth.currentUser;
+    expect(user, isNull);
   });
-//
-//   testWidgets('User can\'t signin with invalid credentials', (WidgetTester tester) async {
-//
-//     final user = MockUser(
-//         isAnonymous: false,
-//         uid: 'uid123',
-//         email: 'test@gmail.com',
-//         displayName: 'ThomasTester'
-//     );
-//
-//     final auth = MockFirebaseAuth(mockUser: user);
-//
-//     // TODO: update for checking UI changes
-//     final result = await auth.signInWithEmailAndPassword(email: 'bad@gmail.com', password: "abc");
-//
-//     final t = result.user;
-// print(result.user);
-//     // Verify 'Send Notification' button exists
-//     expect(t?.displayName, 'ThomasTester');
-//   });
+
+  test('Returns a mocked user if already signed in', () async {
+    final auth = MockFirebaseAuth(signedIn: true, mockUser: tUser);
+    final user = auth.currentUser;
+    expect(user, tUser);
+  });
+
+  test('Returns a hardcoded user token', () async {
+    final auth = MockFirebaseAuth(signedIn: true, mockUser: tUser);
+    final user = auth.currentUser!;
+    final idToken = await user.getIdToken();
+    expect(idToken, isNotEmpty);
+  });
+
+  test('User.reload returns', () async {
+    final auth = MockFirebaseAuth(signedIn: true);
+    final user = auth.currentUser;
+    expect(user, isNotNull);
+    // Does not throw an exception.
+    await user!.reload();
+  });
 }
