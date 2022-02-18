b9ce467bc452083e73592c9ea34e4abf541d0b0d
andrewhaddon74 <86433050+AndrewHaddon74@users.noreply.github.com>
2022-02-18 16:29:00 -0700

    Attempted unit test

---
+++ web-console/tests.py
@@ -0,0 +1,26 @@
+from _pytest.capture import capfd
+
+from Schedule import *
+from django.test import TestCase
+
+#class TestCase(TestCase):
+
+    #def test_Schedule_one(self):
+        #schedule.every(3).seconds.do(background_job).tag('test')
+
+        # Start the background thread
+        #stop_run_continuously = run_continuously()
+
+        #getback = str(schedule.get_jobs('test'))
+        #print(getback)
+
+        # Do some other things...
+        #time.sleep(4)
+
+        # Stop the background thread
+        #stop_run_continuously.set()
+
+        #assert getback == '[Every 3 seconds do background_job() (last run: [never], next run: 2022-02-18 16:26:07)]'
+
+
+
