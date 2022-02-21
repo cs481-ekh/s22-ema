c76fffac3c50cef6bd147d375d26a81c78c2f86b
raosultanate <raosultanate@gmail.com>
2022-02-21 14:08:59 -0700

    connection establised

--- web-console/apps/home/views.py
+++ web-console/apps/home/views.py
@@ -9,12 +9,17 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
 import firebase_admin
+from firebase_admin import firestore
+import os
 
 
 @login_required(login_url="/login/")
 def index(request):
-    context = {'segment': 'index'}
 
+    # Connecting to Firebase
+    connectFirebase()
+
+    context = {'segment': 'index'}
     html_template = loader.get_template('home/index.html')
     return HttpResponse(html_template.render(context, request))
 
@@ -48,4 +53,10 @@ def pages(request):
 # Connecting to firebase
 def connectFirebase():
     # provide file path for firebase credentials
-    cred_obj = firebase_admin.credentials.Certificate('/Users/dnlrao/Desktop/ema-ramen-firebase-adminsdk-7lvc1-97d920871f.json')
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/dnlrao/Desktop/ema-ramen-firebase-adminsdk-7lvc1-97d920871f.json"
+    print("Connected to Firebase!")
+    # Reading (projects) data from the collection
+    db = firestore.Client()
+    docs = db.collection(u'projects').stream()
+    for doc in docs:
+        print(f'{doc.id} => {doc.to_dict()}')
