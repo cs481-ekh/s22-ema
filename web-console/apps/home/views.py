23e3fe1827b5af156e200112556d658c9bcb2d1e
dnlrao <danielrao@u.boisestate.edu>
2022-02-24 12:14:49 -0700

    creating and reading pojects using firebase implemented

--- web-console/apps/home/views.py
+++ web-console/apps/home/views.py
@@ -8,15 +8,18 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
-import firebase_admin
 from firebase_admin import firestore
 import os
+import datetime
 
 
 @login_required(login_url="/login/")
 def index(request):
     context = {'segment': 'index'}
     html_template = loader.get_template('home/index.html')
+    # These tests were conducted
+    read_projects()
+    write_projects("test2", "https://www.facebook.com/", "This is test2", ['abc@gmail.com', 'efg@gmail.com'])
     read_projects()
     return HttpResponse(html_template.render(context, request))
 
@@ -53,13 +56,37 @@ def connect_firebase():
     os.environ[
         "GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/dnlrao/Desktop/ema-ramen-firebase-adminsdk-7lvc1-97d920871f.json"
     print("Connected to Firebase!")
+    db = firestore.Client()
+    return db
 
 
 # Reading (projects) data from the collection
 def read_projects():
     # Connecting to Firebase
-    connect_firebase()
-    db = firestore.Client()
+    db = connect_firebase()
     docs = db.collection(u'projects').stream()
     for doc in docs:
         print(f'{doc.id} => {doc.to_dict()}')
+
+
+# Writing (project) data to firebase
+def write_projects(project_name, survery_link, notes, participants):
+
+    # Connecting to Firebase
+    db = connect_firebase()
+
+    # Collection reference
+    col_ref = db.collection(u'projects')
+
+    # New values to be added
+    new_values = {
+        "dateCreated": datetime.datetime.now(),
+        "desc": notes,
+        "projectId": project_name,
+        "surveryLink": survery_link,
+        "participants": participants
+    }
+
+    # Adding new values to firebase
+    col_ref.document(project_name).create(new_values)
+
