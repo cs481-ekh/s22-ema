ccbf046f10610ec8a249682b588711a09071fb28
dnlrao <danielrao@u.boisestate.edu>
2022-02-25 15:36:06 -0700

    removing unnecessary modules

--- web-console/apps/home/views.py
+++ web-console/apps/home/views.py
@@ -8,7 +8,6 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
-import sys
 from importlib.machinery import SourceFileLoader
 import os
 firebase = SourceFileLoader("fbase", os.getcwd() + "/fbase.py").load_module()
