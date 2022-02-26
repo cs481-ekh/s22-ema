c833b79899fe31fc3340fe41caf1b7ead87c2e1e
AnonymousRecess <Jeffkahn@u.boisestate.edu>
2022-02-26 15:06:00 -0700

    removed unused imports
    Forgot Password

--- lib/screens/forgot_password.dart
+++ lib/screens/forgot_password.dart
@@ -1,7 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:ema/utils/data_classes.dart';
 
 class MyStatefulWidget extends StatefulWidget {
   final TextEditingController usernameController;
