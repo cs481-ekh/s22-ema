f5d814b374b764f5213c9d140bbe13bf00a80dfb
alejandro-macias <matiasale9@gmail.com>
2022-02-28 19:20:56 -0700

    Created and implemented a web-test shell script file

---
+++ web-console/web-test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Exit when any command fails
+set -e
+
+# Keep track of the last executed command
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+# echo an error message before exiting
+trap 'echo "\"${last_command}\" command failed with exit code $?."' EXIT
+
+# Run the unit tests
+python manage.py test
+python manage.py test apps/authentication
+python manage.py test apps/home
+
+# At this point, the commands ran successfully. Therefore, the last exit status is clean trap Exit.
+trap EXIT
