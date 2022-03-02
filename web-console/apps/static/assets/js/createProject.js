0a62eb2d80b170c5d3349395db3b4ff56ba38abd
dnlrao <danielrao@u.boisestate.edu>
2022-03-01 21:59:53 -0700

    fixing template to add participant to the list

--- web-console/apps/static/assets/js/createProject.js
+++ web-console/apps/static/assets/js/createProject.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-
-        // JQuery code to be added in here.
-
+$(document).ready(function () {
+    $("#submitParticipant").click(function () {
+        console.log("hello")
+    });
 });
