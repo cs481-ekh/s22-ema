e8e762ba527118ccb6ec83b165592bd10a89bd2e
alejandro-macias <matiasale9@gmail.com>
2022-03-04 18:08:30 -0700

    configured the url connection of the page to edit projects

--- web-console/core/settings.py
+++ web-console/core/settings.py
@@ -34,7 +34,8 @@ INSTALLED_APPS = [
     'apps.home',  # Enable the inner home (home)
     'apps.API',
     'rest_framework',
-    'apps.create_project'
+    'apps.create_project',
+    'apps.edit_project'
 ]
 
 MIDDLEWARE = [
