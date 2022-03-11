f3b1f37695dda208785a5e7e1fff3cd1730f91a6
alejandro-macias <matiasale9@gmail.com>
2022-03-10 23:15:08 -0700

    Dropdown now lists projects from db on firebase

--- web-console/apps/edit_project/views.py
+++ web-console/apps/edit_project/views.py
@@ -8,16 +8,23 @@ firebase = SourceFileLoader("firebase", os.getcwd() + "/fire_base.py").load_modu
 
 def edit_project(request):
     if request.method == 'POST':
-        print(request.POST.get('selected_project'))
+        # print(request.POST.get('selected_project'))
+
+        # Once we have a project id we want to call a fire base query to get
+        # the project metadata
+        project_id = request.POST.get('selected_project')
+
+        project_dict = firebase.get_project_document_data(project_id)
+
+        print(project_dict)
 
         return render(request, 'home/edit-project.html')
 
     if request.method == 'GET':
 
-        list_of_projects = {
-            "1": "project1",
-            "2": "project2"
-        }
+        list_of_projects = firebase.get_all_project_names()
+
+        print(list_of_projects)
 
         # This grabs the project selected from the front end
         project_selected = request.GET.get('selectedProject')
--- web-console/apps/static/assets/js/edit-project.js
+++ web-console/apps/static/assets/js/edit-project.js
@@ -1,40 +1,22 @@
 $(document).ready(function () {
 
-    //let selectedProject = document.getElementById('selectProjectEditId');
-    //let textOption = selectedProject.options[selectedProject.selectedIndex].text
-    // let survey_link_input_field = document.forms['editProjectInformation']['surveyLink'];
-    // let survey_link_input_field2 = document.forms['editProjectInformation']['surveyLink'];
-    // let edit_project_form = document.getElementById('editProjectInformationId');
-
     // setting up ajax header
     $.ajaxSetup({
         headers: {"X-CSRFToken": Cookies.get("csrftoken")}
     });
 
-    $('#selectProjectEditId').change(function(){
+    $('#selectProjectEditId').change(function () {
         let selectVal = $(this).val()
 
-            $.ajax({
+        $.ajax({
             type: "POST",
             url: '',
             data: {'selected_project': selectVal},
         });
 
 
-    alert($(this).val());
-})
+        alert($(this).val());
+    })
 
 
-    // On the Edit Project page, when the user selects a project,
-    // $("#selectProjectEditId").change(function (event) {
-    //
-    //     let selected_project = $("#selectProjectEditId");
-    //     alert(selectProjectEditId);
-    //
-    //     $.ajax({
-    //         type: "POST",
-    //         url: '',
-    //         data: {'selected_project': selected_project},
-    //     });
-    // });
 });
