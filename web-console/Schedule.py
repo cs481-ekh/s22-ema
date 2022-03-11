49993c0bd2e8b0e76596d6b84a26f2bca8287812
Andrew Haddon <86433050+AndrewHaddon74@users.noreply.github.com>
2022-03-11 12:50:44 -0700

    #49 #146 (#147)
    #49 #146

--- web-console/Schedule.py
+++ web-console/Schedule.py
@@ -1,9 +1,16 @@
 import threading
 import time
+from importlib.machinery import SourceFileLoader
+import os
 import schedule
+import datetime
+from datetime import date
+from datetime import datetime
+
+firebase = SourceFileLoader("firebase", os.getcwd() + "/fire_base.py").load_module()
 
 
-#method to start background thread running the scueding system takes in a int in secends to sleep between checking to see if there are jobs to run defualt 10
+# method to start background thread running the scueding system takes in a int in secends to sleep between checking to see if there are jobs to run defualt 10
 def run_continuously(interval=10):
     """Continuously run, while executing pending jobs at each
     elapsed time interval.
@@ -21,13 +28,13 @@ def run_continuously(interval=10):
         @classmethod
         def run(cls):
             while not cease_continuous_run.is_set():
-                #runs any pending jobs that came up between the last run and now
+                # runs any pending jobs that came up between the last run and now
                 schedule.run_pending()
                 time.sleep(interval)
 
-    #creates thread
+    # creates thread
     continuous_thread = ScheduleThread()
-    #sets thread to deamon so it will die needs to be done before the start
+    # sets thread to deamon so it will die needs to be done before the start
     continuous_thread.daemon = True
     continuous_thread.start()
 
@@ -36,6 +43,66 @@ def background_job():
     print('Hello from the background thread')
 
 
+# Gets the day of the week of the input date
+def get_day_of_week(date):
+    dayofweek = datetime.strptime(date, '%Y-%m-%d').strftime('%A')
+    return dayofweek
 
 
+# creates a job that will only run once at a selected time and date
+def add_reminder_once(date, time, selectedProject, tag):
+    schedule.every().day.at(time).do(run_single_reminder_job, start_date=date, project=selectedProject, tag=tag).tag(
+        tag)
 
+
+# creates a job to be run on a day of the week every week at a selected time
+def add_reminder_weekly(startDate, startTime, endDate, endTime, selectedProject, tag):
+    # get the day of the week and siwtch statment to set based on the day of the week
+    dayofWeek = get_day_of_week(startDate)
+
+    if dayofWeek == 'Monday':
+        schedule.every().monday.at(startTime).until(endDate + ' ' + endTime).do(run_standard_reminder_job,
+                                                                                project=selectedProject).tag(tag)
+    elif dayofWeek == 'Tuesday':
+        schedule.every().tuesday.at(startTime).until(endDate + ' ' + endTime).do(run_standard_reminder_job,
+                                                                                 project=selectedProject).tag(tag)
+    elif dayofWeek == 'Wednesday':
+        schedule.every().wednesday.at(startTime).until(endDate + ' ' + endTime).do(run_standard_reminder_job,
+                                                                                   project=selectedProject).tag(tag)
+    elif dayofWeek == 'Thursday':
+        schedule.every().thursday.at(startTime).until(endDate + ' ' + endTime).do(run_standard_reminder_job,
+                                                                                  project=selectedProject).tag(tag)
+    elif dayofWeek == 'Friday':
+        schedule.every().friday.at(startTime).until(endDate + ' ' + endTime).do(run_standard_reminder_job,
+                                                                                project=selectedProject).tag(tag)
+    elif dayofWeek == 'Saturday':
+        schedule.every().saturday.at(startTime).until(endDate + ' ' + endTime).do(run_standard_reminder_job,
+                                                                                  project=selectedProject).tag(tag)
+    elif dayofWeek == 'Sunday':
+        schedule.every().sunday.at(startTime).until(endDate + ' ' + endTime).do(run_standard_reminder_job,
+                                                                                project=selectedProject).tag(tag)
+    else:
+        print('error')
+
+
+# creates a job to run every day of the week at a selected time
+def add_reminder_daily(startDate, startTime, endDate, endTime, selectedProject, tag):
+    schedule.every().day.at(startTime).until(endDate + ' ' + endTime).do(run_standard_reminder_job,
+                                                                         project=selectedProject).tag(tag)
+
+
+# The single run job this job checks if its the day and if so hits the firebase API then removes itself
+def run_single_reminder_job(start_date, project, tag):
+    current_time = date.today()
+    current_time = current_time.strftime("%Y-%m-%d")
+    parsed_start_date = datetime.strptime(start_date, "%Y-%m-%d").date()
+    if str(current_time) == str(parsed_start_date):
+        # return schedule.cancel_job()
+        # run the job to hit the API
+        loaded_project = firebase.get_project_document_data(project)
+        print(loaded_project)
+
+
+# standard job for daily and weekly jobs, will be written in another task
+def run_standard_reminder_job(project):
+    print('x')
--- web-console/apps/Reminders/views.py
+++ web-console/apps/Reminders/views.py
@@ -1,11 +1,63 @@
+from importlib.machinery import SourceFileLoader
+import os
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+firebase = SourceFileLoader("firebase", os.getcwd() + "/fire_base.py").load_module()
+Schedule = SourceFileLoader("Schedule", os.getcwd() + "/Schedule.py").load_module()
+
+
+# view for Notfication setting
 @login_required(login_url="/login/")
 def index(request):
-    if(request.method == 'POST'):
-        print('test')
-        print(list(request.POST.items()))
-        return render(request, 'home/notification-settings.html')
+    list_of_projects = firebase.get_all_project_names()
+    error = []
+    flag = False
+    if request.method == 'POST':
+        # separating out incoming variable
+        selectedProject = request.POST.get('selectedProject')
+        startDate = request.POST.get('startDate')
+        startTime = request.POST.get('startTime')
+        selection = request.POST.get('selection')
+        endDate = request.POST.get('endDate')
+        endTime = request.POST.get('endTime')
+        print(selectedProject)
+
+        # Checks to ensure there is a end date/time if the program does not run once
+        if selection != 'Once':
+            if endDate == '' or endTime == '':
+                # should add an error that returns if there is an issue based on how the selection box is done
+                error.append('For Daily and Weekly Notifications please enter an expiration date and time!')
+                flag = True
+
+        if startDate == '' or startTime == '':
+            error.append('Please enter a start date and a time for notification!')
+            flag = True
+
+        if selectedProject == 'Select Project':
+            error.append('Please select a project to add a reminder to!')
+            flag = True
+
+        if flag:
+            print(error)
+            return render(request, 'home/notification-settings.html',
+                          {'list_of_projects_dict': list_of_projects, 'error_code': error})
+
+        # Selection is once add one reminder to system
+        if selection == 'Once':
+            Schedule.add_reminder_once(startDate, startTime, selectedProject, '')
+            return render(request, 'home/notification-settings.html',
+                          {'list_of_projects_dict': list_of_projects, 'error_code': error})
+
+        # if the selection is weekly
+        if selection == 'Weekly':
+            Schedule.add_reminder_weekly(startDate, startTime, endDate, endTime, selectedProject, '')
+
+        # if the selection is Daily
+        if selection == 'Daily':
+            Schedule.add_reminder_daily(startDate, startTime, endDate, endTime, selectedProject, '')
+
+    # returns the same view of the notfication page no matter what
+    return render(request, 'home/notification-settings.html',
+                  {'list_of_projects_dict': list_of_projects, 'error_code': error})
