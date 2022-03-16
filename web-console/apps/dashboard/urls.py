228c62eb02767c2675779a3e3dc2b5eb6fbd1ec6
danielrao <danielrao@u.boisestate.edu>
2022-03-16 17:28:53 -0600

    added Dashboard page and fixed UI

--- web-console/apps/dashboard/urls.py
+++ web-console/apps/dashboard/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard, name='dashboard')
+    path('', views.index, name='dashboard')
+    # Matches any html file
+    # re_path(r'^.*\.*', views.pages, name='pages')
 ]
--- web-console/core/urls.py
+++ web-console/core/urls.py
@@ -7,8 +7,6 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-
-
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.urls import urlpatterns
@@ -16,10 +14,9 @@ from rest_framework.urls import urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),          # Django admin route
-    path("", include("apps.authentication.urls")), # Auth routes - login / register
-    path("", include("apps.home.urls")),            # UI Kits Html files
+    path("", include("apps.authentication.urls")), # Auth routes - login / register / forgot password
+    path("", include("apps.dashboard.urls")),           # main dashboard
     path("createProject/", include("apps.create_project.urls")),
     path("editProject/", include("apps.edit_project.urls")),
     path("reminders/", include("apps.Reminders.urls")),
-    path("dashboard/", include("apps.dashboard.urls"))
 ]
