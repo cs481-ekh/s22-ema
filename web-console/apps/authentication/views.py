6abee319a889a926f7239a729042027adc70c3d0
alejandro-macias <matiasale9@gmail.com>
2022-03-15 23:12:50 -0600

    The reset password link functions

--- web-console/apps/authentication/views.py
+++ web-console/apps/authentication/views.py
@@ -7,6 +7,7 @@ Copyright (c) 2019 - present AppSeed.us
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm, SignUpForm
+from django.contrib.auth import get_user_model
 
 
 def login_view(request):
@@ -59,4 +60,35 @@ def register_user(request):
 
 
 def reset_password(request):
+    # Grabbing the value of our input fields
+    username = request.POST.get('user_name')
+    user_password = request.POST.get('user-password')
+    user_confirm_password = request.POST.get('user-confirm-password')
+
+    # Security check before changing the password
+    if username is not None and user_password is not None and user_confirm_password is not None:
+
+        # If the both password input fields match, then proceed with the password change
+        if user_password == user_confirm_password:
+            user_model = get_user_model()
+
+            # This will help us determine if the admin user exists in database
+            count = user_model.objects.filter(username=username).count()
+
+            # If count is 0, then the admin user does not exist
+            if count == 0:
+                return render(request, "home/user-not-exist.html")
+            # Admin user exists, proceed with updating password
+            else:
+                user = user_model.objects.get(username=username)
+                user.set_password(user_password)
+                user.save()
+
+                # Authenticating user aka login user
+                user = authenticate(username=username, password=user_password)
+                login(request, user)
+
+                return render(request,
+                              "home/login.html")  # Needs to be changed later to direct to dashboard/  once dashboard is implemented
+
     return render(request, "home/auth-reset-pass.html")
