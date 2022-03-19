ca704f61eab6dc2c576e9762a5d7f42f5bce6db3
danielrao <danielrao@u.boisestate.edu>
2022-03-18 20:36:59 -0600

    added drop down box to select projects

--- web-console/apps/dashboard/views.py
+++ web-console/apps/dashboard/views.py
@@ -13,7 +13,10 @@ firebase = SourceFileLoader("firebase", os.getcwd() + "/fire_base.py").load_modu
 
 @login_required(login_url="/login/")
 def index(request):
-    return render(request, "home/index.html")
+    if request.method == 'GET':
+        list_of_projects = firebase.get_all_project_names()
+        print(list_of_projects)
+        return render(request, 'home/index.html', {'list_of_projects_dict': list_of_projects})
 
 
 # To be removed later
