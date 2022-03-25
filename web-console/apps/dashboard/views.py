c8aca393b198e1b766c7cd929393ddff926d8765
danielrao <danielrao@u.boisestate.edu>
2022-03-25 01:14:50 -0700

    made percentage code functional for the card on Dashboard

--- web-console/apps/dashboard/views.py
+++ web-console/apps/dashboard/views.py
@@ -13,6 +13,28 @@ firebase = SourceFileLoader("firebase", os.getcwd() + "/fire_base.py").load_modu
 
 @login_required(login_url="/login/")
 def index(request):
+    # POST only comes in if drop down is value is changed
+    if request.method == 'POST':
+        # Getting project name from POST
+        proj_name = request.POST.get("selectedProject")
+        # Safe check for project name
+        if proj_name is not None and proj_name != 'Select':
+            # Getting participant list from firebase present in the project
+            part_list = firebase.get_participant_list(proj_name)
+            part_list_count = len(part_list)
+
+            # percentage to be displayed on the card
+            participant_percentage = (part_list_count / len(firebase.get_all_users_names())) * 100
+
+            # Setting cookie on client end for the data to be represented:
+            response = HttpResponse("Cookie Set")
+            # set part_list_count on client end for the data to be represented
+            response.set_cookie('part_list_count', part_list_count)
+            # setting percentage cookie to be displayed on card
+            response.set_cookie('participant_percentage', participant_percentage)
+            return response
+
+    # Contains list of projects
     list_of_projects = firebase.get_all_project_names()
 
     # total projects present on firebase
@@ -20,9 +42,6 @@ def index(request):
 
     # total users present on firebase
     list_of_users_count = len(firebase.get_all_users_names())
-
-    if request.method == 'POST':
-        print(request.POST)
 
     return render(request, 'home/index.html',
                   {'list_of_projects_dict': list_of_projects, 'list_of_projects_count': list_of_projects_count,
