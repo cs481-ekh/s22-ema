4d3ae6921f096f4302292dec7096463748c1dbd0
alejandro-macias <matiasale9@gmail.com>
2022-04-02 13:39:37 -0600

    When dropdown selection is equal to Select Project then disable fields, reset selection field's data, and disable button

--- web-console/apps/static/assets/js/notification-settings.js
+++ web-console/apps/static/assets/js/notification-settings.js
@@ -5,6 +5,28 @@ $(document).ready(function () {
         headers: {"X-CSRFToken": Cookies.get("csrftoken")}
     });
 
-    
+    // When the drop down value is "Select Project"
+    $(document).on('change', '#selectedProjectId', function () {
+        // if no project is selected
+        if (document.getElementById("selectedProjectId").value == "Select Project") {
+
+            // disable all selection fields
+            document.getElementById("scheduleSendDateInput").disabled = true;
+            document.getElementById("scheduleSendDateTimeInput").disabled = true;
+            document.getElementById("whenToNotifySelectionId").disabled = true;
+            document.getElementById("expirationDateInput").disabled = true;
+            document.getElementById("expirationTimeInput").disabled = true;
+
+            // disable set notification btn
+            document.getElementById("setNotificationBtnId").disabled = true;
+
+            // Reset data in selection fields
+            document.getElementById("scheduleSendDateInput").value = "";
+            document.getElementById("scheduleSendDateTimeInput").value = "";
+            document.getElementById("whenToNotifySelectionId").value = "Once";
+            document.getElementById("expirationDateInput").value = "";
+            document.getElementById("expirationTimeInput").value = "";
+        }
+    });
 
 });
