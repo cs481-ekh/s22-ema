0c67dfd9e9bec3383f80b2d9ecc90a179cf3ad24
danielrao <danielrao@u.boisestate.edu>
2022-04-08 21:12:02 -0600

    commented credentials to prevent build fail

--- web-console/manage.py
+++ web-console/manage.py
@@ -30,7 +30,7 @@ def main():
     # Firebase
     ##################################################################################################################
     # provide file path for firebase credentials (Needs to be taken out during build [CI])
-    os.environ[
+    # os.environ[
 
         # "GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/rogue/Downloads/ema-ramen-firebase-adminsdk-7lvc1-97d920871f.json"
 
@@ -38,7 +38,7 @@ def main():
 
         # "GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/brandonboys/desktop/ema-final/ema-ramen-firebase-adminsdk-7lvc1-97d920871f.json"
 
-        "GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/danielrao/Desktop/ema-ramen-firebase-adminsdk-7lvc1-97d920871f.json"
+        #"GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/danielrao/Desktop/ema-ramen-firebase-adminsdk-7lvc1-97d920871f.json"
 
     # # initializing app using credentials
     # firebase_admin.initialize_app(credentials.Certificate(os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")))
