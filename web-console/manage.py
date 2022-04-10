a8f33dde90793af3f38131b737f4558665e576ac
danielrao <danielrao@u.boisestate.edu>
2022-04-10 16:36:31 -0600

    deletes reminderBackup data as the project gets deleted

--- web-console/manage.py
+++ web-console/manage.py
@@ -38,7 +38,7 @@ def main():
 
         # "GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/brandonboys/desktop/ema-final/ema-ramen-firebase-adminsdk-7lvc1-97d920871f.json"
 
-        #"GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/danielrao/Desktop/ema-ramen-firebase-adminsdk-7lvc1-97d920871f.json"
+        # "GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/danielrao/Desktop/ema-ramen-firebase-adminsdk-7lvc1-97d920871f.json"
 
     # # initializing app using credentials
     # firebase_admin.initialize_app(credentials.Certificate(os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")))
