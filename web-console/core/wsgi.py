0bcf1c49aa2c9fee80fbacf8b8e06f68a72c99c4
dnlrao <54600096+dnlrao@users.noreply.github.com>
2022-04-19 10:12:52 -0600

    updated wsgi to prevent failure on docker container

--- web-console/core/wsgi.py
+++ web-console/core/wsgi.py
@@ -15,7 +15,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "ema-ramen-firebase-adminsdk-7lvc1-97d920871f.json"
 os.environ["GOOGLE_EMAIL_CREDENTIALS"] = "google_email_creds.txt"
 firebase_admin.initialize_app(credentials.Certificate(os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")))
-run_continuously()
+Schedule.run_continuously()
 Schedule.firebase_DueDate_Perge()
 Schedule.dabatBaseReload()
 
